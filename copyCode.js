0302dd15b16d2fc3df6626c48d5d09c16e3df868
Vamsi Krishna Avula <vamsi_ism@outlook.com>
2015-09-13 18:02:00 +0530

    User script to add a copy button after 'pre' tags.
    Copy code with a single click from various programming sites.

---
+++ copyCode.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Copy Code
+// @version      0.1
+// @description  Adds a copy button after 'pre' tags.
+// @author       Vamsi Krishna | vamsi_ism@outlook.com
+// @match        *://*/*
+// ==/UserScript==
+
+onload = function(){
+	function insertAfter(referenceNode, newNode){
+		referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+	}
+
+	function copy(event){
+		var range = document.createRange();
+		range.selectNode(pre[this.id]);
+
+		var sel = getSelection();
+		sel.removeAllRanges(); // removing selection without backup
+		sel.addRange(range);
+
+		var success = document.execCommand('copy');
+		sel.removeAllRanges(); // possible TODO
+
+		if (success){
+			this.innerText = "Copied!";
+			this.setAttribute("style", "cursor:pointer");
+			setTimeout(
+				function(but){
+					but.innerText = "Copy";
+					but.setAttribute("style", "cursor:pointer");
+				},
+				196, this
+			)
+		}
+	}
+
+	var pre = document.getElementsByTagName('pre');
+	var but, att;
+
+	for (var i = 0; i < pre.length; i++){
+		but = document.createElement("button");
+		but.innerText = "Copy"
+
+		att = document.createAttribute("id");
+		att.value = i;
+		but.setAttributeNode(att);
+
+		att = document.createAttribute("style");
+		att.value = "cursor:pointer";
+		but.setAttributeNode(att);
+
+		insertAfter(pre[i], but);
+		but.addEventListener('click', copy);
+	}
+}
