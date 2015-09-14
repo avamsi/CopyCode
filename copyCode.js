a193f43370c0b16f4e34673e00fd395b573722f5
Vamsi Krishna Avula <vamsi_ism@outlook.com>
2015-09-14 22:31:23 +0530

    changed onload to eventlistener

--- copyCode.js
+++ copyCode.js
@@ -7,51 +7,47 @@
 // @version      0.1
 // ==/UserScript==
 
-onload = function(){
-	function insertAfter(referenceNode, newNode){
-		referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+function insertAfter(referenceNode, newNode){
+	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function copy(event){
+	var range = document.createRange();
+	range.selectNode(pre[this.id]);
+
+	var sel = getSelection();
+	sel.removeAllRanges(); // removing selection without backup
+	sel.addRange(range);
+
+	var success = document.execCommand('copy');
+	sel.removeAllRanges(); // possible TODO
+
+	if (success){
+		this.innerText = 'Copied!';
+		this.setAttribute('style', 'cursor:pointer');
+		setTimeout(
+			function(but){
+				but.innerText = 'Copy';
+				but.setAttribute('style', 'cursor:pointer');
+			},
+			196, this
+		)
 	}
+}
 
-	function copy(event){
-		var range = document.createRange();
-		range.selectNode(pre[this.id]);
-
-		var sel = getSelection();
-		sel.removeAllRanges(); // removing selection without backup
-		sel.addRange(range);
-
-		var success = document.execCommand('copy');
-		sel.removeAllRanges(); // possible TODO
-
-		if (success){
-			this.innerText = "Copied!";
-			this.setAttribute("style", "cursor:pointer");
-			setTimeout(
-				function(but){
-					but.innerText = "Copy";
-					but.setAttribute("style", "cursor:pointer");
-				},
-				196, this
-			)
-		}
-	}
-
+function main(){
 	var pre = document.getElementsByTagName('pre');
 	var but, att;
 
 	for (var i = 0; i < pre.length; i++){
-		but = document.createElement("button");
-		but.innerText = "Copy"
-
-		att = document.createAttribute("id");
-		att.value = i;
-		but.setAttributeNode(att);
-
-		att = document.createAttribute("style");
-		att.value = "cursor:pointer";
-		but.setAttributeNode(att);
+		but = document.createElement('button');
+		but.innerText = 'Copy'
+		but.setAttribute('id', i)
+		but.setAttribute('style', 'cursor:pointer');
 
 		insertAfter(pre[i], but);
 		but.addEventListener('click', copy);
 	}
 }
+
+window.addEventListener('load', main)
