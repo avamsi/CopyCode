f806a45fb922b0557c1195a94f9989d5a7a9c25e
Vamsi Krishna Avula <vamsi_ism@outlook.com>
2015-09-15 13:43:24 +0530

    bug fix

--- copyCode.js
+++ copyCode.js
@@ -7,35 +7,35 @@
 // @version      0.1
 // ==/UserScript==
 
-function insertAfter(referenceNode, newNode){
-	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
-function copy(event){
-	var range = document.createRange();
-	range.selectNode(pre[this.id]);
-
-	var sel = getSelection();
-	sel.removeAllRanges(); // removing selection without backup
-	sel.addRange(range);
-
-	var success = document.execCommand('copy');
-	sel.removeAllRanges(); // possible TODO
-
-	if (success){
-		this.innerText = 'Copied!';
-		this.setAttribute('style', 'cursor:pointer');
-		setTimeout(
-			function(but){
-				but.innerText = 'Copy';
-				but.setAttribute('style', 'cursor:pointer');
-			},
-			196, this
-		)
-	}
-}
-
 function main(){
+	function insertAfter(referenceNode, newNode){
+		referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+	}
+
+	function copy(event){
+		var range = document.createRange();
+		range.selectNode(pre[this.id]);
+
+		var sel = getSelection();
+		sel.removeAllRanges(); // removing selection without backup
+		sel.addRange(range);
+
+		var success = document.execCommand('copy');
+		sel.removeAllRanges(); // possible TODO
+
+		if (success){
+			this.innerText = 'Copied!';
+			this.setAttribute('style', 'cursor:pointer');
+			setTimeout(
+				function(but){
+					but.innerText = 'Copy';
+					but.setAttribute('style', 'cursor:pointer');
+				},
+				196, this
+			)
+		}
+	}
+
 	var pre = document.getElementsByTagName('pre');
 	var but, att;
 
